3b5ea123e05561f598ef0266d5c7ff9da822cadb
Evgeny Zhuravlev <e.zhuravlev@ventra.ru>
2013-08-17 12:28:13 +0100

    fixed hashCode()

--- jsmpp/src/main/java/org/jsmpp/bean/SarSegmentSeqnum.java
+++ jsmpp/src/main/java/org/jsmpp/bean/SarSegmentSeqnum.java
@@ -47,6 +47,7 @@ public class SarSegmentSeqnum {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
+        result = prime * result + value;
         return result;
     }
 
